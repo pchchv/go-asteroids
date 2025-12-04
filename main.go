6a3afd5fcdb5d7d5c0af2fcb15248725f2cf8431
pchchv <ipchchv@gmail.com>
2025-12-04 20:00:32 +0300

    feat: add update and draw implement functions

--- main.go
+++ main.go
@@ -17,3 +17,16 @@ func init() {
 func deinit() {
 	rl.CloseWindow()
 }
+
+func draw() {
+	rl.BeginDrawing()
+	// set the background to black
+	rl.ClearBackground(rl.Black)
+	// draw the score to the screen
+	rl.DrawText("Score 0", 10, 10, 20, rl.Gray)
+	rl.EndDrawing()
+
+}
+
+// TODO: update the state
+func update() {}
