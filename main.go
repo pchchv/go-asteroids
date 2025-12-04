db5903b6ac62e88304110cf33662242c14ef5d92
pchchv <ipchchv@gmail.com>
2025-12-04 19:58:25 +0300

    feat: implement init and deinit functions

--- main.go
+++ main.go
@@ -1,2 +1,19 @@
 package main
 
+import rl "github.com/gen2brain/raylib-go/raylib"
+
+const (
+	screenWidth  = 800
+	screenHeight = 400
+)
+
+func init() {
+	// builtin go function which runs before main()
+	// setup the raylib window
+	rl.InitWindow(screenWidth, screenHeight, "Asteroids")
+	rl.SetTargetFPS(60)
+}
+
+func deinit() {
+	rl.CloseWindow()
+}
