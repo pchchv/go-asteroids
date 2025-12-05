bc9cd7eed19a56bc1be1003ea300d403643fde90
pchchv <ipchchv@gmail.com>
2025-12-05 09:24:49 +0300

    style: rebuild to mono file

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"math"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 const (
-	tileSize     = 64
-	screenWidth  = 800
-	screenHeight = 400
+	tileSize      = 64
+	screenWidth   = 800
+	screenHeight  = 400
+	rotationSpeed = 2.0
+	playerSpeed   = 6.0
 )
 
 var (
@@ -14,6 +20,70 @@ var (
 	texBackground rl.Texture2D
 	spriteRec     rl.Rectangle
 )
+
+type Player struct {
+	isBoosting   bool
+	rotation     float32
+	acceleration float32
+	position     rl.Vector2
+	speed        rl.Vector2
+	size         rl.Vector2
+}
+
+func (p *Player) Draw() {
+	destTexture := rl.Rectangle{X: p.position.X, Y: p.position.Y, Width: p.size.X, Height: p.size.Y}
+	rl.DrawTexturePro(
+		texTiles,
+		spriteRec,
+		destTexture,
+		rl.Vector2{X: p.size.X / 2, Y: p.size.Y / 2},
+		p.rotation,
+		rl.White,
+	)
+}
+
+func (p *Player) Update() {
+	// rotate the player with the arrow keys
+	if rl.IsKeyDown(rl.KeyLeft) {
+		player.rotation -= rotationSpeed
+	}
+
+	if rl.IsKeyDown(rl.KeyRight) {
+		player.rotation += rotationSpeed
+	}
+
+	// accelerate the player with up
+	if rl.IsKeyDown(rl.KeyUp) {
+		if player.acceleration < 0.9 {
+			player.acceleration += 0.1
+		}
+	}
+
+	// decellerate the player with down
+	if rl.IsKeyDown(rl.KeyDown) {
+		if player.acceleration > 0 {
+			player.acceleration -= 0.05
+		}
+
+		if player.acceleration < 0 {
+			player.acceleration = 0
+
+		}
+	}
+
+	// get the direction the sprite is pointing
+	direction := getDirectionVector(player.rotation)
+
+	// start to move to the direction
+	player.speed = rl.Vector2Scale(direction, playerSpeed)
+
+	// accelerate in that direction
+	player.position.X += player.speed.X * player.acceleration
+	player.position.Y -= player.speed.Y * player.acceleration
+
+	// to void losing our ship, wrap around the screen
+	wrapPosition(&p.position, tileSize)
+}
 
 func init() {
 	// builtin go function which runs before main()
@@ -64,6 +134,36 @@ func initGame() {
 		rotation:     0.0,
 		acceleration: 0.0,
 		isBoosting:   false,
+	}
+}
+
+func getDirectionVector(rotation float32) rl.Vector2 {
+	// convert the rotation to radians
+	radians := float64(rotation) * rl.Deg2rad
+
+	// return the vector of the direction we are pointing at
+	return rl.Vector2{
+		X: float32(math.Sin(radians)),
+		Y: float32(math.Cos(radians)),
+	}
+}
+
+func wrapPosition(pos *rl.Vector2, objectSize float32) {
+	// if it goes off the left side of the screen
+	if pos.X > screenWidth+objectSize {
+		pos.X = -objectSize
+	}
+	// if it goes off the right side of the screen
+	if pos.X < -objectSize {
+		pos.X = screenWidth + objectSize
+	}
+	// if it goes off the bottom of the screen
+	if pos.Y > screenHeight+objectSize {
+		pos.Y = -objectSize
+	}
+	// if it goes off the top of the screen
+	if pos.Y < -objectSize {
+		pos.Y = screenHeight + objectSize
 	}
 }
 
