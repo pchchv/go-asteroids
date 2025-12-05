76d98f07a0efa57bf491004f4d98b7e980421efd
pchchv <ipchchv@gmail.com>
2025-12-05 09:21:23 +0300

    feat: implement Update method for Player

--- player.go
+++ player.go
@@ -2,6 +2,11 @@ package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
+const (
+	rotationSpeed = 2.0
+	playerSpeed   = 6.0
+)
+
 type Player struct {
 	isBoosting   bool
 	rotation     float32
@@ -21,4 +26,47 @@ func (p *Player) Draw() {
 		p.rotation,
 		rl.White,
 	)
+}
+
+func (p *Player) Update() {
+	// rotate the player with the arrow keys
+	if rl.IsKeyDown(rl.KeyLeft) {
+		player.rotation -= rotationSpeed
+	}
+
+	if rl.IsKeyDown(rl.KeyRight) {
+		player.rotation += rotationSpeed
+	}
+
+	// accelerate the player with up
+	if rl.IsKeyDown(rl.KeyUp) {
+		if player.acceleration < 0.9 {
+			player.acceleration += 0.1
+		}
+	}
+
+	// decellerate the player with down
+	if rl.IsKeyDown(rl.KeyDown) {
+		if player.acceleration > 0 {
+			player.acceleration -= 0.05
+		}
+
+		if player.acceleration < 0 {
+			player.acceleration = 0
+
+		}
+	}
+
+	// get the direction the sprite is pointing
+	direction := getDirectionVector(player.rotation)
+
+	// start to move to the direction
+	player.speed = rl.Vector2Scale(direction, playerSpeed)
+
+	// accelerate in that direction
+	player.position.X += player.speed.X * player.acceleration
+	player.position.Y -= player.speed.Y * player.acceleration
+
+	// to void losing our ship, wrap around the screen
+	wrapPosition(&p.position, tileSize)
 }
