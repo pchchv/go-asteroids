a00219b49c34f9d87ba34fcdfbeddd4d0414c4de
pchchv <ipchchv@gmail.com>
2025-12-05 09:20:01 +0300

    feat: implement wrapPosition function

--- helpers.go
+++ helpers.go
@@ -16,3 +16,22 @@ func getDirectionVector(rotation float32) rl.Vector2 {
 		Y: float32(math.Cos(radians)),
 	}
 }
+
+func wrapPosition(pos *rl.Vector2, objectSize float32) {
+	// if it goes off the left side of the screen
+	if pos.X > screenWidth+objectSize {
+		pos.X = -objectSize
+	}
+	// If it goes off the right side of the screen
+	if pos.X < -objectSize {
+		pos.X = screenWidth + objectSize
+	}
+	// If it goes off the bottom of the screen
+	if pos.Y > screenHeight+objectSize {
+		pos.Y = -objectSize
+	}
+	// If it goes off the top of the screen
+	if pos.Y < -objectSize {
+		pos.Y = screenHeight + objectSize
+	}
+}
